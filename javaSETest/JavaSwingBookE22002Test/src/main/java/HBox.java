43a476d89bbc1afb952749caee5cdf1416bd24d5
YunZhang2014 <jslgdxzy@gmail.com>
2015-09-13 07:16:51 +0930

    add files

---
+++ javaSETest/JavaSwingBookE22002Test/src/main/java/HBox.java
@@ -0,0 +1,34 @@
+
+// HBox.java
+// A quick test of the BoxLayout manager using the Box utility class.
+//
+import java.awt.Button;
+import java.awt.Panel;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+
+public class HBox extends JFrame {
+
+	private static final long serialVersionUID = 6483680529739015493L;
+
+	public HBox() {
+		super("Horizontal Box Test Frame");
+		setSize(200, 100);
+		Panel box = new Panel();
+
+		// Use BoxLayout.Y_AXIS below if you want a vertical box
+		box.setLayout(new BoxLayout(box, BoxLayout.Y_AXIS));
+		setContentPane(box);
+		for (int i = 0; i < 3; i++) {
+			Button b = new Button("B" + i);
+			box.add(b);
+		}
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+	public static void main(String args[]) {
+		HBox bt = new HBox();
+	}
+}
