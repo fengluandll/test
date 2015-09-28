4e51a1b4b15c8054a7943459789dc712b8081127
YunZhang2014 <jslgdxzy@gmail.com>
2015-09-28 14:13:21 +0930

    delete somefiles

--- hiberanteTest/JavaPersistencewithHibernateE12006Test/ch02/src/hello/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package hello;
-
-/**
- * Created by Yun on 27/09/2015.
- */
-public class HelloWorld {
-}
--- hiberanteTest/JavaPersistencewithHibernateE12006Test/ch02/src/persistence/HibernateUtil.java
+++
@@ -1,7 +0,0 @@
-package persistence;
-
-/**
- * Created by Yun on 27/09/2015.
- */
-public class HibernateUtil {
-}
