4c1a07e1a79909b0c6125af7b1851e83536c2de4
YunZhang2014 <jslgdxzy@gmail.com>
2015-09-29 20:44:29 +0930

    add file

---
+++ androidTest/androidProgrammingTheBigNerdRanch2013/ch07/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -0,0 +1,29 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CrimeActivity extends AppCompatActivity implements CrimeFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        if (fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer,fragment).commit();
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        System.out.println("CrimeActivity.onFragmentInteraction");
+    }
+}
