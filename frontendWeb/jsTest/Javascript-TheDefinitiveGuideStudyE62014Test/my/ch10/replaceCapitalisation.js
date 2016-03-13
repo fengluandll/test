232958fdae679bac7e781622aba4d4ed0a21380e
YunZhang2014 <jslgdxzy@gmail.com>
2016-03-13 16:20:16 +1030

    some test

---
+++ frontendWeb/jsTest/Javascript-TheDefinitiveGuideStudyE62014Test/my/ch10/replaceCapitalisation.js
@@ -0,0 +1,4 @@
+var text = "I hated Javascript before. But I love javascript now.";
+var newText = text.replace(/JavaScript/gi,"JavaScript");
+console.log(text);
+console.log(newText);
---
+++ frontendWeb/jsTest/Javascript-TheDefinitiveGuideStudyE62014Test/my/ch10/search.js
@@ -0,0 +1,1 @@
+console.log("JavaScript".search(/script/i));
