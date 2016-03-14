9bd14c89c14183c4077c4ebc613bd50506ae4e3b
YunZhang2014 <jslgdxzy@gmail.com>
2016-03-14 10:55:28 +1030

    Add first component

--- frontendWeb/react.jsTest/React.jsEssentials2015Test/my/c03/snapterest/source/app.js
+++ frontendWeb/react.jsTest/React.jsEssentials2015Test/my/c03/snapterest/source/app.js
@@ -2,7 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var ReactClass = React.createClass({
     render: function () {
-        return React.createElement('h1', { className: 'header' }, 'React Component');
+        //return React.createElement('h1', { className: 'header' }, 'React Component');
+        return false;
     }
 });
 var reactComponentElement = React.createElement(ReactClass);
