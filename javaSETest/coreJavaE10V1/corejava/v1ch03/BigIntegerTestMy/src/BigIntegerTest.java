1e7b16f903371ece9fde7fac84263d686be7163e
YunZhang2014 <jslgdxzy@gmail.com>
2016-04-06 10:39:37 +0930

    change

---
+++ javaSETest/coreJavaE10V1/corejava/v1ch03/BigIntegerTestMy/src/BigIntegerTest.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * Created by Allbts-IT on 6/04/2016.
+ */
+public class BigIntegerTest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("How many numbers do you need to draw? ");
+        int k = in.nextInt();
+        System.out.print("What is the highest number you can draw? ");
+        int n = in.nextInt();
+    }
+}
