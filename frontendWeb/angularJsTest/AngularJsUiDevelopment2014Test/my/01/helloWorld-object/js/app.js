608508f38e19eb8acfbc59b1e34738d707847a76
YunZhang2014 <jslgdxzy@gmail.com>
2016-05-16 23:19:48 +0930

    object

---
+++ frontendWeb/angularJsTest/AngularJsUiDevelopment2014Test/my/01/helloWorld-object/js/app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Allbts-IT on 16/05/2016.
+ */
+'use strict';
+
+angular.module('myApp',['myApp.controllers']);
---
+++ frontendWeb/angularJsTest/AngularJsUiDevelopment2014Test/my/01/helloWorld-object/js/controllers.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Allbts-IT on 16/05/2016.
+ */
+'use strict';
+
+angular.module('myApp.controllers',[]).
+    controller('helloWorldCtrl',function ($scope) {
+    $scope.name= {first: "Jane", last: "Doe"};
+});
