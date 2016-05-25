9fb1e6d7b9896f3d01f68822a0e84b26bcb43ab3
YunZhang2014 <jslgdxzy@gmail.com>
2016-05-26 00:19:58 +0930

    add files

---
+++ javaSETest/mongoDbJavaTest/mongoDbJavaBookTest/MongoDbforJavaDevelopers2015Test/my/ch06/helloSpringBoot/src/main/java/SimpleBean.java
@@ -0,0 +1,12 @@
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Allbts-IT on 25/05/2016.
+ */
+@Component
+public class SimpleBean {
+    @Override
+    public String toString() {
+        return "Hello , This is an example component";
+    }
+}
