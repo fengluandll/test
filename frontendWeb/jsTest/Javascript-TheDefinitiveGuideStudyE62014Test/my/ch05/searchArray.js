bc61f2877fb84fd66da37b20c264c792e6976dab
YunZhang2014 <jslgdxzy@gmail.com>
2016-06-24 00:54:24 +0930

    search in an array

---
+++ frontendWeb/jsTest/Javascript-TheDefinitiveGuideStudyE62014Test/my/ch05/searchArray.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Allbts-IT on 24/06/2016.
+ */
+'use strict';
+
+    var a = [10,8,6];
+    var target = 11;
+    for (var i = 0 ; i < a.length ; i++) {
+        if (a[i] == target) break;
+    }
+console.log('The index is ' + i);
