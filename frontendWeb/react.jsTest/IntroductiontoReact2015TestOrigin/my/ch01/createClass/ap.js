4b952b0192a03a9338daa7cee02230e13069f24e
YunZhang2014 <jslgdxzy@gmail.com>
2017-02-01 08:38:24 +1030

    IC

--- frontendWeb/react.jsTest/IntroductiontoReact2015TestOrigin/my/ch01/createClass/ap.js
+++ frontendWeb/react.jsTest/IntroductiontoReact2015TestOrigin/my/ch01/createClass/ap.js
@@ -1,3 +1,11 @@
-/**
- * Created by YunZhang on 26/1/17.
- */
+var MyComponent = React.createClass({
+    render: function () {
+        return (
+            <div>
+                {this.props.name}
+            </div>
+                );
+    }
+});
+
+React.render(<MyComponent name="yun" />, document.getElementById('container'));
