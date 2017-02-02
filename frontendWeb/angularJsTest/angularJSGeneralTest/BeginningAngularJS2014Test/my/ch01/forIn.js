8199f72f10b4fe72c5e42cc6d0f2ca69eff50931
YunZhang2014 <jslgdxzy@gmail.com>
2017-02-03 09:51:11 +1030

    change

---
+++ frontendWeb/angularJsTest/angularJSGeneralTest/BeginningAngularJS2014Test/my/ch01/forIn.js
@@ -0,0 +1,8 @@
+var myObject = {
+    firstName: "Andrew",
+    surName: "Grant",
+    age: 21
+};
+
+for (var prop in myObject)
+    console.log(myObject[prop]);
---
+++ frontendWeb/angularJsTest/angularJSGeneralTest/BeginningAngularJS2014Test/my/ch01/numString.js
@@ -0,0 +1,3 @@
+var val7 = 25;
+var val8 = "25";
+console.log(val7 + val8);
