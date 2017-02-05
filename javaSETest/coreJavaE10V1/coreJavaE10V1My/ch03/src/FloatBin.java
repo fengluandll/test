0317e6f8acb5b9af48ac6d031e21ed8d7f489dfd
YunZhang2014 <jslgdxzy@gmail.com>
2017-02-05 14:16:30 +1030

    change

---
+++ javaSETest/coreJavaE10V1/coreJavaE10V1My/ch03/src/FloatBin.java
@@ -0,0 +1,9 @@
+/**
+ * Created by YunZhang on 5/2/17.
+ */
+public class FloatBin {
+    public static void main(String[] args) {
+        double x = 0x1.0p-3;
+        System.out.println(x); //=>0.125
+    }
+}
