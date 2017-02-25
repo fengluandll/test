24d7b40607c20f24a261927aee7499720d2b1d6e
YunZhang2014 <jslgdxzy@gmail.com>
2017-02-25 15:42:08 +1030

    change

--- frontendWeb/reactNativeTest/MasteringReactNative2017Test/my/ch03/FlexBoxDemo/index.ios.js
+++ frontendWeb/reactNativeTest/MasteringReactNative2017Test/my/ch03/FlexBoxDemo/index.ios.js
@@ -4,50 +4,48 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
+const FlexBoxLayout = () => (
+    <View style={styles.container}>
+        <View style={styles.item}/>
+        <View style={styles.item}/>
+        <View style={styles.item}/>
+    </View>
+);
+
 export default class FlexBoxDemo extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <FlexBoxLayout/>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        // justifyContent: 'flex-start',
+        // justifyContent: 'flex-end',
+        // justifyContent: 'center',
+        // justifyContent: 'space-between',
+        justifyContent: 'space-around',
+        alignItems: 'flex-start',
+        flexDirection: 'column',
+    },
+    item:{
+        backgroundColor: 'lightgoldenrodyellow',
+        borderWidth: 1,
+        borderColor: 'goldenrod',
+        height: 150,
+        width: 150,
+    }
 });
 
 AppRegistry.registerComponent('FlexBoxDemo', () => FlexBoxDemo);
