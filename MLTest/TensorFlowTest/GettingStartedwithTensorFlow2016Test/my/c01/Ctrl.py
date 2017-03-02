1bf668b340a7e57e18285324207711835e289b23
YunZhang2014 <jslgdxzy@gmail.com>
2017-03-03 00:43:28 +1030

    IC

---
+++ MLTest/TensorFlowTest/GettingStartedwithTensorFlow2016Test/my/c01/Ctrl.py
@@ -0,0 +1,6 @@
+rangeList = range(10)
+print rangeList;
+
+for number in rangeList:
+    if number in (3,4,7,9):
+        # "Break" ends
---
+++ MLTest/TensorFlowTest/GettingStartedwithTensorFlow2016Test/my/c01/Str.py
@@ -0,0 +1,6 @@
+print "Nome: %s\nNumber:%s\nString: %s" % ("Bear,",3,3*"-");
+
+strString = """this is a string
+on multiple lines."""
+
+print "This is %(verbC)s un %(name)s" % {"name": "test","verb":"is"}
