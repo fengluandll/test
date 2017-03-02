c9a7caf5cdcfb458469658765f50c4943c4aa07d
YunZhang2014 <jslgdxzy@gmail.com>
2017-03-02 14:19:38 +1030

    change

--- phpTest/phpGeneralTest/LearningPHP2016Test/LearningPHP2016TestMy/ch01/hello/hello.php
+++ phpTest/phpGeneralTest/LearningPHP2016Test/LearningPHP2016TestMy/ch01/hello/hello.php
@@ -1,17 +1,4 @@
-<html>
 
-<head>
-    <title>
-        PHP says hello
-    </title>
-</head>
-
-<body>
-    <b>
         <?php
             print "Hello,World"
         ?>
-    </b>
-</body>
-
-</html>
