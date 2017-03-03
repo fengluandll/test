025f59b5578c3e3b3366c03f72b72f16f33ab361
YunZhang2014 <jslgdxzy@gmail.com>
2017-03-03 11:16:38 +1030

    change

--- MLTest/TensorFlowTest/GettingStartedwithTensorFlow2016Test/my/c01/Ctrl.py
+++ MLTest/TensorFlowTest/GettingStartedwithTensorFlow2016Test/my/c01/Ctrl.py
@@ -2,5 +2,18 @@ rangeList = range(10)
 print rangeList;
 
 for number in rangeList:
-    if number in (3,4,7,9):
-        # "Break" ends
+    if number in (3, 4, 7, 9):
+        break
+    else:
+        continue
+else:
+    pass
+
+if rangeList[1] == 2:
+    print "the second element (lists are 0-based) is 2"
+elif rangeList[1] == 3:
+    print "the second element is 3"
+else:
+    print "I don't know"
+while rangeList[1] == 1:
+    pass
