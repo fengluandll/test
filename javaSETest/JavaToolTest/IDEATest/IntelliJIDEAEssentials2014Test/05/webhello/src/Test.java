975fd4e84796a67a7bc7fc1735d06ca49e373e44
YunZhang2014 <jslgdxzy@gmail.com>
2017-08-09 14:55:26 +0930

    change

---
+++ javaSETest/JavaToolTest/IDEATest/IntelliJIDEAEssentials2014Test/05/webhello/src/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    private String name;
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("bear");
+
+        double random = Math.random();
+
+        for (int i = 0; i < 100; i++) {
+            long milis = System.currentTimeMillis();
+            System.out.println(milis);
+        }
+
+//
+    }
+}
