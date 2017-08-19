01d7905cb1da1b815bdb4a3b96b84b096a55deb9
YunZhang2014 <jslgdxzy@gmail.com>
2017-08-19 22:08:35 +0930

    change

---
+++ frontendWeb/jsTest/JasmineJavaScriptTesting2015Test/my/ch03/spec/NewInvestmentViewSpec.js
@@ -0,0 +1,9 @@
+describe("NewInvestmentView",function () {
+    it("should allow the input of the stock symbol",function () {
+
+    });
+
+    it("should allow the input of shares", function() { });
+
+    it("should allow the input of the share price", function() { });
+});
---
+++ frontendWeb/jsTest/JasmineJavaScriptTesting2015Test/my/ch03/src/NewInvestmentView.js
@@ -0,0 +1,8 @@
+(function ($,Investment,Stock) {
+    //'use strict';
+    function NewInvestmentView(params) {
+
+    }
+
+    this.NewInvestmentView = NewInvestmentView;
+})(jQuery,Investment,Stock);
