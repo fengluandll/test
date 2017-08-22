d09c3060ac356443c26384a5bfffcba42486bfd3
YunZhang2014 <jslgdxzy@gmail.com>
2017-08-22 20:30:32 +0930

    add webstorm test

---
+++ frontendWeb/angularJsTest/AngularJS-UpandRunning2014Test/my/03/webstormtemplate/index.js
@@ -0,0 +1,14 @@
+directive('', function factory() {
+    var directiveDefinitionObject = {
+        
+        compile
+    :
+    function compile(tElement, tAttrs, transclude) {
+        
+        return function (scope, element, attrs) {
+        }
+    }
+}
+    ;
+    return directiveDefinitionObject;
+})
