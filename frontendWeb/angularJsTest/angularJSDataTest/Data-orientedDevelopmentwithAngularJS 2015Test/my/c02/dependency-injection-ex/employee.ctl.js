5772912ac37b61d1a3b647aac355af9e42fce9da
YunZhang2014 <jslgdxzy@gmail.com>
2017-08-26 10:46:21 +0930

    ic

---
+++ frontendWeb/angularJsTest/angularJSDataTest/Data-orientedDevelopmentwithAngularJS 2015Test/my/c02/dependency-injection-ex/employee.ctl.js
@@ -0,0 +1,7 @@
+"use strict";
+
+app.controller('EmployeeCtrl',['$scope','employeeSvc',function ($scope,employeeSvc) {
+    $scope.employeeData = {
+        employees: employeeSvc.getEmployees()
+    };
+}]);
---
+++ frontendWeb/angularJsTest/angularJSDataTest/Data-orientedDevelopmentwithAngularJS 2015Test/my/c02/dependency-injection-ex/employee.svc.js
@@ -0,0 +1,12 @@
+"use strict";
+
+app.service('employeeSvc',function () {
+    var Employee = function (name, age) { this.name = name; this.age = age; };
+
+    var getEmployees = function () {
+        return [ new Employee("First employee", 56), new Employee("Second employee", 44), new Employee("Last employee", 32) ];
+    };
+
+    this.Employee = Employee;
+    this.getEmployees = getEmployees;
+});
