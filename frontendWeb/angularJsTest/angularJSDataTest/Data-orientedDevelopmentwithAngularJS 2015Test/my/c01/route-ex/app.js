2a6cdfefbd52de37d33e8f201294d68595fa9c82
YunZhang2014 <jslgdxzy@gmail.com>
2017-08-26 10:34:12 +0930

    change

---
+++ frontendWeb/angularJsTest/angularJSDataTest/Data-orientedDevelopmentwithAngularJS 2015Test/my/c01/route-ex/app.js
@@ -0,0 +1,12 @@
+var app = angular.module('routeApp',['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider.when('/employees',{
+        templateUrl: 'employee.tpl.html',
+        controller: 'EmployeeCtrl'
+    }).when('/departments',{
+        templateUrl: 'department.tpl.html', controller: 'DepartmentCtrl'
+    }).otherwise({
+        redirectTo: '/'
+    });
+});
