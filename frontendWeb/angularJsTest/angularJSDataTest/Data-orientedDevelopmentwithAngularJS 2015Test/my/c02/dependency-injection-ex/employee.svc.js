786d5254e9233ea433a1a9cfb8bc842747aa6420
YunZhang2014 <jslgdxzy@gmail.com>
2017-08-26 10:57:43 +0930

    ic

--- frontendWeb/angularJsTest/angularJSDataTest/Data-orientedDevelopmentwithAngularJS 2015Test/my/c02/dependency-injection-ex/employee.svc.js
+++ frontendWeb/angularJsTest/angularJSDataTest/Data-orientedDevelopmentwithAngularJS 2015Test/my/c02/dependency-injection-ex/employee.svc.js
@@ -1,10 +1,15 @@
 "use strict";
 
-app.service('employeeSvc',function () {
-    var Employee = function (name, age) { this.name = name; this.age = age; };
+app.service('employeeSvc', function () {
+    var Employee = function (name, age) {
+        this.name = name;
+        this.age = age;
+    };
 
     var getEmployees = function () {
-        return [ new Employee("First employee", 56), new Employee("Second employee", 44), new Employee("Last employee", 32) ];
+        return [new Employee("First employee", 56),
+            new Employee("Second employee", 44),
+            new Employee("Last employee", 32)];
     };
 
     this.Employee = Employee;
