290b33e494627e7da1bfc1a96ce2a1cd5e7ada29
YunZhang2014 <jslgdxzy@gmail.com>
2017-08-28 07:15:02 +0930

    change

---
+++ frontendWeb/angularJsTest/angularJSDataTest/Data-orientedDevelopmentwithAngularJS 2015Test/my/c03/and-scope-ex/employee.dir.js
@@ -0,0 +1,9 @@
+app.directive('myEmployee',function () {
+    return {
+        restrict: 'E',
+        scope:{
+            'click': '&onClick'
+        },
+        templateUrl: 'employee.tpl.html'
+    };
+});
