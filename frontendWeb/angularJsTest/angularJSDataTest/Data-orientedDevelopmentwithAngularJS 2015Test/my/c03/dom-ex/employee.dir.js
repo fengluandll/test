f1dbc94452d816fd8fd62092ca1b49a9024dd535
YunZhang2014 <jslgdxzy@gmail.com>
2017-08-28 07:58:03 +0930

    change

---
+++ frontendWeb/angularJsTest/angularJSDataTest/Data-orientedDevelopmentwithAngularJS 2015Test/my/c03/dom-ex/employee.dir.js
@@ -0,0 +1,9 @@
+app.directive('myEmployee',function () {
+    return {
+        link: function (scope,element,attrs) {
+            var name = scope.firstName + ' ' + attrs.lastname;
+            element.text(name);
+            $(element).effect('shake');
+        }
+    };
+});
