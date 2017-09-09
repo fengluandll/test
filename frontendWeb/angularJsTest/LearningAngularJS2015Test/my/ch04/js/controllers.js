5182ba08f53f89ea0759b2215f2cc9de4a1179c6
YunZhang2014 <jslgdxzy@gmail.com>
2017-09-09 17:54:09 +0930

    to run the karma unit tests

--- frontendWeb/angularJsTest/LearningAngularJS2015Test/my/ch04/js/controllers.js
+++ frontendWeb/angularJsTest/LearningAngularJS2015Test/my/ch04/js/controllers.js
@@ -4,17 +4,17 @@ var helloWorldControllers = angular.module('helloWorldControllers', []);
 
 helloWorldControllers.controller('MainCtrl', ['$scope',
     function MainCtrl($scope) {
-        $scope.message = "Hello,MainCtrl";
+        $scope.message = "Hello World";
     }
 ]);
 
 helloWorldControllers.controller("ShowCtrl",['$scope',function ($scope) {
-    $scope.message = "show the world";
+    $scope.message = "Show The World";
 }]);
 
 helloWorldControllers.controller('CustomerCtrl',['$scope',
 function ($scope) {
-    $scope.customerName = 'Yun Zhang';
+    $scope.customerName = "Bob's Burgers";
     $scope.customerNumber = '33522';
 
     $scope.changeCustomer = function () {
