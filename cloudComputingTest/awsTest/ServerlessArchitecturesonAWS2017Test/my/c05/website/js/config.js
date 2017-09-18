38992f22dfd15246fa802116c3ab68c733ec3ba7
YunZhang2014 <jslgdxzy@gmail.com>
2017-09-18 14:23:53 +0930

    change

---
+++ cloudComputingTest/awsTest/ServerlessArchitecturesonAWS2017Test/my/c05/website/js/config.js
@@ -0,0 +1,7 @@
+var configConstants = {
+	auth0: {
+		domain: 'jackzhang.au.auth0.com',
+		clientId: 'Qg6QKRzIJbm7uvpi3Q9EQTaBltSsxdY6'
+	},
+  apiBaseUrl: 'https://c5d2jlvt30.execute-api.ap-southeast-2.amazonaws.com/dev'
+};
