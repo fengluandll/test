f9040b539d92164ad916f93dd6bc0c7e72c3e939
yunzhang <jslgdxzy@gmail.com>
2018-03-17 19:16:45 +1030

    change

---
+++ oplTest/pythonTest/pythonGenetalTest/IntroductiontoComputingUsingPythonE22015Test/my/c03/helloInput.py
@@ -0,0 +1,9 @@
+first = input('Enter your first name: ')
+
+last = input( 'Enter your last name: ');
+
+line1 = 'Hello '+ first + ' ' + last + '...'
+
+print(line1)
+
+print( 'Welcome to the world of Python!')
