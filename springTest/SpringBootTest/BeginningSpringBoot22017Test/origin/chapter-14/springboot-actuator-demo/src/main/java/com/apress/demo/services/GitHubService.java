4c17be57f8342e7260afa673108ca66d7e338d88
Yun Zhang <jslgdxzy@gmail.com>
2018-04-26 07:37:15 +0930

    change

--- springTest/SpringBootTest/BeginningSpringBoot22017Test/origin/chapter-14/springboot-actuator-demo/src/main/java/com/apress/demo/services/GitHubService.java
+++ springTest/SpringBootTest/BeginningSpringBoot22017Test/origin/chapter-14/springboot-actuator-demo/src/main/java/com/apress/demo/services/GitHubService.java
@@ -4,7 +4,7 @@
 package com.apress.demo.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.metrics.GaugeService;
+//import org.springframework.boot.actuate.metrics.GaugeService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -18,8 +18,8 @@ import com.apress.demo.models.GitHubUser;
 @Service
 public class GitHubService {
 
-	@Autowired
-	GaugeService gaugeService;
+//	@Autowired
+//	GaugeService gaugeService;
 	
 	public GitHubUser getUserInfo(String username)
 	{
@@ -30,7 +30,7 @@ public class GitHubService {
 			long start = System.currentTimeMillis();
 			gitHubUser = restTemplate.getForObject(url , GitHubUser.class);
 			long end = System.currentTimeMillis();
-			gaugeService.submit("gauge.guthub.response-time", (end-start));
+//			gaugeService.submit("gauge.guthub.response-time", (end-start));
 		} catch (RestClientException e) {
 			e.printStackTrace();
 		}
--- springTest/SpringBootTest/BeginningSpringBoot22017Test/origin/chapter-14/springboot-actuator-demo/src/main/java/com/apress/demo/services/LoginService.java
+++ springTest/SpringBootTest/BeginningSpringBoot22017Test/origin/chapter-14/springboot-actuator-demo/src/main/java/com/apress/demo/services/LoginService.java
@@ -4,7 +4,7 @@
 package com.apress.demo.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.metrics.CounterService;
+//import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,16 +14,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class LoginService {
 
-	@Autowired
-	private CounterService counterService;
+//	@Autowired
+//	private CounterService counterService;
 	
 	public boolean login(String email, String password)
 	{
 		if("admin@gmail.com".equalsIgnoreCase(email) && "admin".equals(password)){
-			counterService.increment("counter.login.success");
+//			counterService.increment("counter.login.success");
 			return true;
 		} else {
-			counterService.increment("counter.login.failure");
+//			counterService.increment("counter.login.failure");
 			return false;
 		}
 	}
