26425dd1a967c4c93c49760fd6d49d1950242b63
Yun Zhang <jslgdxzy@gmail.com>
2018-07-17 22:02:53 +0930

    change

---
+++ hibernateTest/BeginningHibernateE42016Test/BeginningHibernateE42016TestMy/chapter01/src/main/java/chapter01/pojo/Message.java
@@ -0,0 +1,39 @@
+package chapter01.pojo;
+
+public class Message {
+
+    private Long id;
+
+    String text;
+
+    public Message() {
+    }
+
+    public Message(String text) {
+        setText(text);
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
