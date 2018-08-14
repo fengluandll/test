62cd0081ef5b9afb47833fe0359e414b3c046925
YunZhang2014 <jslgdxzy@gmail.com>
2018-08-14 15:42:47 +0930

    change

---
+++ groovyTest/ProgrammingGroovy22013Test/src/c02/Greetings.groovy
@@ -0,0 +1,12 @@
+package c02
+
+public class Greetings {
+    public static void main(String[] args) {
+
+        for (i in 0..2) {
+            print 'ho '
+        }
+
+        println("Merry Groovy!")
+    }
+}
---
+++ groovyTest/ProgrammingGroovy22013Test/src/c02/waysToLoop.groovy
@@ -0,0 +1,9 @@
+package c02
+
+0.upto(2){print "$it "}
+
+println()
+3.times {print "$it "}
+
+println()
+0.step(10, 2) {print "$it "}
