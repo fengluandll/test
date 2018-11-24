389ab8935e9ad8b390b7733008d07aeacb70ec22
Yun Zhang <jslgdxzy@gmail.com>
2018-11-24 16:33:50 +1030

    add menus

--- assign-troy/assign5q1/src/main/java/Main.java
+++ assign-troy/assign5q1/src/main/java/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("ui/mainStage.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
