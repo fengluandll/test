b8fca52d001b9f29fb79467bc8bc3a10905367c0
Yun <yun.zhang5918@gmail.com>
2018-11-27 17:15:44 +1030

    change

---
+++ groovyTest/GroovyinActionE22015Test/my/src/c01/SearchWord.groovy
@@ -0,0 +1,15 @@
+package c01
+
+import static groovyx.gpars.GParsPool.withPool
+
+def urls = [
+        'http://www.groovy-lang.org',
+        'http://gpars.codehaus.org',
+        'http://gr8conf.org/'
+]*.toURI();
+
+println withPool {
+    urls.collectParallel {
+        it.text.findAll(~/[Gg]roovy/).size()
+    }
+}
---
+++ groovyTest/GroovyinActionE22015Test/my/src/c02/ClinkClosure.groovy
@@ -0,0 +1,12 @@
+package c02
+
+def totalClinks = 0
+def partyPeople = 100
+
+1.upto(partyPeople) { guestNumber ->
+    clinksWithGuest = guestNumber - 1
+    totalClinks += clinksWithGuest
+}
+
+assert totalClinks == partyPeople * (partyPeople - 1) / 2
+
