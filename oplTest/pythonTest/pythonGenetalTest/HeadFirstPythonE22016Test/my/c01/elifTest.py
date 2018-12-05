aa6c7c22b291335acea89277856ec24b3a06c37b
Yun Zhang <jslgdxzy@gmail.com>
2018-12-05 23:31:25 +1030

    change

---
+++ oplTest/pythonTest/pythonGenetalTest/HeadFirstPythonE22016Test/my/c01/elifTest.py
@@ -0,0 +1,8 @@
+print("Please input the day of week")
+today = input()
+if today == 'Saturday':
+    print('Party!!')
+elif today == 'Sunday':
+    print('Recover')
+else:
+    print('work work work')
