b7aeb772e5deb499456a4314411295a277002d5c
Yun <yun.zhang5918@gmail.com>
2019-02-01 12:06:42 +1030

    change

--- swDesignTest/OAuthTest/OAuth2inAction2017Test/original/exercises/ch-11-ex-5/authorizationServer.js
+++ swDesignTest/OAuthTest/OAuth2inAction2017Test/original/exercises/ch-11-ex-5/authorizationServer.js
@@ -249,7 +249,51 @@ app.post('/revoke', function(req, res) {
 	/*
 	 * Implement the token revocation endpoint
 	 */
-	
+	var auth = req.headers['authorization'];
+	if (auth) {
+		// check the auth header
+		var clientCredentials = decodeClientCredentials(auth);
+		var clientId = clientCredentials.id;
+		var clientSecret = clientCredentials.secret;
+	}
+
+	// otherwise, check the post body
+	if (req.body.client_id) {
+		if (clientId) {
+			// if we've already seen the client's credentials in the authorization header, this is an error
+			console.log('Client attempted to authenticate with multiple methods');
+			res.status(401).json({error: 'invalid_client'});
+			return;
+		}
+
+		var clientId = req.body.client_id;
+		var clientSecret = req.body.client_secret;
+	}
+
+	var client = getClient(clientId);
+	if (!client) {
+		console.log('Unknown client %s', clientId);
+		res.status(401).json({error: 'invalid_client'});
+		return;
+	}
+
+	if (client.client_secret != clientSecret) {
+		console.log('Mismatched client secret, expected %s got %s', client.client_secret, clientSecret);
+		res.status(401).json({error: 'invalid_client'});
+		return;
+	}
+
+	var inToken = req.body.token;
+	nosql.remove(function(token) {
+		if (token.access_token == inToken && token.client_id == clientId) {
+			return true;
+		}
+	}, function(err, count) {
+		console.log("Removed %s tokens", count);
+		res.status(204).end();
+		return;
+	});
+
 });
 
 var buildUrl = function(base, options, hash) {
--- swDesignTest/OAuthTest/OAuth2inAction2017Test/original/exercises/ch-3-ex-1/client.js
+++ swDesignTest/OAuthTest/OAuth2inAction2017Test/original/exercises/ch-3-ex-1/client.js
@@ -115,6 +115,26 @@ app.get('/fetch_resource', function(req, res) {
 		res.render('error', {error: 'Missing access token.'});
 		return;
 	}
+
+	console.log('Making request with access token %s', access_token);
+
+	var headers = {
+		'Authorization': 'Bearer ' + access_token
+	};
+
+	var resource = request('POST', protectedResource,
+		{headers: headers}
+	);
+
+	if (resource.statusCode >= 200 && resource.statusCode < 300) {
+		var body = JSON.parse(resource.getBody());
+		res.render('data', {resource: body});
+		return;
+	} else {
+		access_token = null;
+		res.render('error', {error: resource.statusCode});
+		return;
+	}
 	
 });
 
