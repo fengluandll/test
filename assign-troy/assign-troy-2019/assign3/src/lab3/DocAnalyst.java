50ef87f3b109baefeac1ccbc945e641308486e77
Yun Zhang <jslgdxzy@gmail.com>
2019-02-16 13:47:59 +1030

    finish the the job

--- assign-troy/assign-troy-2019/assign3/src/lab3/DocAnalyst.java
+++ assign-troy/assign-troy-2019/assign3/src/lab3/DocAnalyst.java
@@ -1,7 +1,26 @@
 package lab3;
 
+import java.util.Scanner;
+
 public class DocAnalyst {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter file name");
+        String fileName = scanner.nextLine();
+        if (fileName == null || fileName.length() == 0) {
+            System.out.println("file name cannot be empty");
+        } else if (fileName.endsWith(".csv")) {
+            CSVDoc csvDoc = new CSVDoc(fileName);
+            csvDoc.readFile();
+            csvDoc.collectDocInfo();
+            String message = fileName + " has " + csvDoc.rowCount + " rows and " + csvDoc.columnCount + " columns";
+            System.out.println(message);
+        } else {
+            RegularDoc regularDoc = new RegularDoc(fileName);
+            regularDoc.readFile();
+            regularDoc.collectDocInfo();
+            System.out.println("This file has " + regularDoc.wordCount + " words");
+        }
+        scanner.close();
     }
 }
