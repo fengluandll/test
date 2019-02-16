4672fe780083afdb1baaf3f92ac33be3bc473ca1
Yun Zhang <jslgdxzy@gmail.com>
2019-02-16 13:31:31 +1030

    CSVDoc: analyse the rowCount and columnCount

--- assign-troy/assign-troy-2019/assign3/src/lab3/CSVDoc.java
+++ assign-troy/assign-troy-2019/assign3/src/lab3/CSVDoc.java
@@ -1,8 +1,8 @@
 package lab3;
 
 public class CSVDoc extends Document {
-    int columnCount;
-    int rowCount;
+    int columnCount = 0;
+    int rowCount = 0;
 
     CSVDoc(String filename) {
         super(filename);
@@ -10,6 +10,16 @@ public class CSVDoc extends Document {
 
     @Override
     void collectDocInfo() {
-
+        // get the lines file fileContent
+        if (fileContent != null) {
+            String[] lines = fileContent.toString().split("\\s*\n");
+            rowCount = lines.length;
+            // get the column count
+            if (lines.length > 0 && lines[0] != null) {
+                String firstLine = lines[0];
+                String[] columns = firstLine.split("\\s*,\\s*");
+                columnCount = columns.length;
+            }
+        }
     }
 }
--- assign-troy/assign-troy-2019/assign3/src/lab3/DocAnalyst.java
+++ assign-troy/assign-troy-2019/assign3/src/lab3/DocAnalyst.java
@@ -1,4 +1,7 @@
 package lab3;
 
 public class DocAnalyst {
+    public static void main(String[] args) {
+
+    }
 }
