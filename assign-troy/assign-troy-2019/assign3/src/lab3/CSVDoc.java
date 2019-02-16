56b8bfc193bc81a558be1d1e69674e9ebc032256
Yun Zhang <jslgdxzy@gmail.com>
2019-02-16 13:17:26 +1030

    fix the test compilation errors

---
+++ assign-troy/assign-troy-2019/assign3/src/lab3/CSVDoc.java
@@ -0,0 +1,15 @@
+package lab3;
+
+public class CSVDoc extends Document {
+    int columnCount;
+    int rowCount;
+
+    CSVDoc(String filename) {
+        super(filename);
+    }
+
+    @Override
+    void collectDocInfo() {
+
+    }
+}
---
+++ assign-troy/assign-troy-2019/assign3/src/lab3/RegularDoc.java
@@ -0,0 +1,14 @@
+package lab3;
+
+public class RegularDoc extends Document {
+    int wordCount;
+
+    RegularDoc(String filename) {
+        super(filename);
+    }
+
+    @Override
+    void collectDocInfo() {
+
+    }
+}
