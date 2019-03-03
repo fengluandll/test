29208e1532c42165b005544e304e21290a2eee2a
Yun Zhang <jslgdxzy@gmail.com>
2019-03-04 08:45:14 +1030

    pass test 6

--- assign-troy/assign-troy-2019/lab4/src/lab4/IceCream.java
+++ assign-troy/assign-troy-2019/lab4/src/lab4/IceCream.java
@@ -1,8 +1,16 @@
 package lab4;
 
 public class IceCream extends Food {
+
+    final static int ICECREAM_CALORIES = 500;
+
+    public IceCream() {
+        calories += ICECREAM_CALORIES;
+        System.out.println("Serving Ice cream");
+    }
+
     @Override
     public String eat() {
-        return null;
+        return "Slurp Slurp";
     }
 }
