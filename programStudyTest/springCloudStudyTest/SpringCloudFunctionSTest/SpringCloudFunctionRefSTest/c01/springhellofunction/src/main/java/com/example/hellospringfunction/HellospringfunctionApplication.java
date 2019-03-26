dc6b2a36c647647004d60d675382880a95006cbe
Yun Zhang <jslgdxzy@gmail.com>
2019-03-27 02:31:50 +1030

    ic

---
+++ programStudyTest/springCloudStudyTest/SpringCloudFunctionSTest/SpringCloudFunctionRefSTest/c01/springhellofunction/src/main/java/com/example/hellospringfunction/HellospringfunctionApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellospringfunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellospringfunctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellospringfunctionApplication.class, args);
+    }
+
+}
