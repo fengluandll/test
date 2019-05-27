c622b7cf63e6222bf34bf5bd2bd9528ae9d96658
Yun <yun.zhang5918@gmail.com>
2019-05-27 12:31:29 +0930

    change

--- javaSETest/rxJava/rxJava2OfficialTest/src/main/java/operators/FlatMapMaybeTest.java
+++ javaSETest/rxJava/rxJava2OfficialTest/src/main/java/operators/FlatMapMaybeTest.java
@@ -1,7 +1,22 @@
 package operators;
 
+
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
+
 public class FlatMapMaybeTest {
     public static void main(String[] args) {
+        Observable.just(9.0, 16.0, -4.0)
+                .flatMapMaybe( x -> {
+                    if (x.compareTo(0.0) < 0) {
+                        return Maybe.empty();
+                    } else {
+                        return Maybe.just(Math.sqrt(x));
+                    }
+                }).subscribe(
+                System.out::println,
+                Throwable::printStackTrace,
+                () -> System.out.println("onComplete"));
 
     }
 }
