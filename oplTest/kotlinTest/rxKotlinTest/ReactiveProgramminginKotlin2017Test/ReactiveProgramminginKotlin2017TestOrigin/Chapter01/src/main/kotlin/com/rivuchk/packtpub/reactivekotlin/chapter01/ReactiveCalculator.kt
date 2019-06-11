f4f918771e28ed9402665fea53f6429219c49340
Yun <yun.zhang5918@gmail.com>
2019-06-11 12:24:16 +0930

    change

--- oplTest/kotlinTest/rxKotlinTest/ReactiveProgramminginKotlin2017Test/ReactiveProgramminginKotlin2017TestOrigin/Chapter01/src/main/kotlin/com/rivuchk/packtpub/reactivekotlin/chapter01/ReactiveCalculator.kt
+++ oplTest/kotlinTest/rxKotlinTest/ReactiveProgramminginKotlin2017Test/ReactiveProgramminginKotlin2017TestOrigin/Chapter01/src/main/kotlin/com/rivuchk/packtpub/reactivekotlin/chapter01/ReactiveCalculator.kt
@@ -1,5 +1,8 @@
 package com.rivuchk.packtpub.reactivekotlin.chapter01
 
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
 
 /**
  * Created by Rivu Chakraborty on 7/3/2017.
@@ -56,33 +59,33 @@ class ReactiveCalculator(a:Int, b:Int) {
     fun modifyNumbers (a:Int = nums.first, b: Int = nums.second) {
         nums = Pair(a,b)
         subjectCalc.onNext(this)
-
     }
 
     fun handleInput(inputLine:String?) {
         if(!inputLine.equals("exit")) {
-            val pattern: java.util.regex.Pattern = java.util.regex.Pattern.compile("([a|b])(?:\\s)?=(?:\\s)?(\\d*)");
+            val pattern: java.util.regex.Pattern = Pattern
+                    .compile("([a|b])(?:\\s)?=(?:\\s)?(\\d*)")
 
             var a: Int? = null
             var b: Int? = null
 
-            val matcher: java.util.regex.Matcher = pattern.matcher(inputLine)
+            val matcher: Matcher = pattern.matcher(inputLine)
 
-            if (matcher.matches() && matcher.group(1) != null && matcher.group(2) != null) {
-                if(matcher.group(1).toLowerCase().equals("a")){
+            if (matcher.matches()
+                    && matcher.group(1) != null
+                    && matcher.group(2) != null) {
+                if(matcher.group(1).toLowerCase() == "a"){
                     a = matcher.group(2).toInt()
-                } else if(matcher.group(1).toLowerCase().equals("b")){
+                } else if(matcher.group(1).toLowerCase() == "b"){
                     b = matcher.group(2).toInt()
                 }
             }
-
 
             when {
                 a != null && b != null -> modifyNumbers(a, b)
                 a != null -> modifyNumbers(a = a)
                 b != null -> modifyNumbers(b = b)
                 else -> println("Invalid Input")
-
             }
         }
     }
@@ -92,9 +95,7 @@ class ReactiveCalculator(a:Int, b:Int) {
 
 fun main(args: Array<String>) {
     println("Initial Out put with a = 15, b = 10")
-    var calculator: com.rivuchk.packtpub.reactivekotlin.chapter1.ReactiveCalculator = com.rivuchk.packtpub.reactivekotlin.chapter1.ReactiveCalculator(15, 10)
-
-
+    var calculator: ReactiveCalculator = ReactiveCalculator(15, 10)
 
     println("Enter a = <number> or b = <number> in separate lines\nexit to exit the program")
     var line:String?
