2145c456379be6e264dab47a85fa1cc4122536ed
Yun <yun.zhang5918@gmail.com>
2019-06-11 12:58:32 +0930

    change

--- oplTest/kotlinTest/rxKotlinTest/ReactiveProgramminginKotlin2017Test/ReactiveProgramminginKotlin2017TestOrigin/Chapter02/src/main/kotlin/com/rivuchk/packtpub/reactivekotlin/chapter02/HighOrderFuncInlineExmpl.kt
+++ oplTest/kotlinTest/rxKotlinTest/ReactiveProgramminginKotlin2017Test/ReactiveProgramminginKotlin2017TestOrigin/Chapter02/src/main/kotlin/com/rivuchk/packtpub/reactivekotlin/chapter02/HighOrderFuncInlineExmpl.kt
@@ -1,21 +1,22 @@
-//package com.rivuchk.packtpub.reactivekotlin.chapter02
-//
-//import com.rivuchk.packtpub.reactivekotlin.isEven
-//
-///**
-// * Created by Rivu Chakraborty on 11-07-2017.
-// */
-//
-//
-//inline fun highOrderFuncInline(a:Int, validityCheckFunc:(a:Int)->Boolean) {//(1)
-//    if(validityCheckFunc(a)) {//(2)
-//        println("a $a is Valid")
-//    } else {
-//        println("a $a is Invalid")
-//    }
-//}
-//
-//fun main(args: Array<String>) {
-//    highOrderFuncInline(12,{ a:Int -> a.isEven()})//(3)
-//    highOrderFuncInline(19,{ a:Int -> a.isEven()})
-//}
+package com.rivuchk.packtpub.reactivekotlin.chapter02
+
+/**
+ * Created by Rivu Chakraborty on 11-07-2017.
+ */
+
+
+inline fun highOrderFuncInline(a:Int,
+           validityCheckFunc:(a:Int)->Boolean) {//(1)
+    if(validityCheckFunc(a)) {//(2)
+        println("a $a is Valid")
+    } else {
+        println("a $a is Invalid")
+    }
+}
+
+fun main(args: Array<String>) {
+    highOrderFuncInline(12) { a:Int -> a.isEven()}//(3)
+    highOrderFuncInline(19) { a:Int -> a.isEven()}
+}
+
+fun Int.isEven(): Boolean = this % 2 == 0
--- oplTest/kotlinTest/rxKotlinTest/ReactiveProgramminginKotlin2017Test/ReactiveProgramminginKotlin2017TestOrigin/Chapter02/src/main/kotlin/com/rivuchk/packtpub/reactivekotlin/chapter02/InlineExmple.kt
+++ oplTest/kotlinTest/rxKotlinTest/ReactiveProgramminginKotlin2017Test/ReactiveProgramminginKotlin2017TestOrigin/Chapter02/src/main/kotlin/com/rivuchk/packtpub/reactivekotlin/chapter02/InlineExmple.kt
@@ -1,9 +1,5 @@
 package com.rivuchk.packtpub.reactivekotlin.chapter02
 
-/**
- * Created by Rivu Chakraborty on 11-07-2017.
- */
-
 inline fun doSomeStuff(a:Int = 0) = a+(a*a)
 
 fun main(args: Array<String>) {
