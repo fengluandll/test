784b87e30f015eb736665b82ff3e71a5feae9fdc
Yun <yun.zhang5918@gmail.com>
2019-06-11 16:29:34 +0930

    change

--- oplTest/kotlinTest/kotlinMicroserviceTest/Hands-OnMicroservicesWithKotlin2018TestOrigin/Chapter02/src/main/kotlin/com/microservices/chapter2/ExampleService.kt
+++ oplTest/kotlinTest/kotlinMicroserviceTest/Hands-OnMicroservicesWithKotlin2018TestOrigin/Chapter02/src/main/kotlin/com/microservices/chapter2/ExampleService.kt
@@ -3,6 +3,7 @@ package com.microservices.chapter2
 import org.springframework.beans.factory.annotation.Value
 
 class ExampleService : ServiceInterface {
+
   @Value(value = "\${service.message.text}")
   private lateinit var text: String
 
--- oplTest/kotlinTest/rxKotlinTest/ReactiveProgramminginKotlin2017Test/ReactiveProgramminginKotlin2017TestOrigin/Chapter02/src/main/kotlin/com/rivuchk/packtpub/reactivekotlin/chapter02/Monads.kt
+++ oplTest/kotlinTest/rxKotlinTest/ReactiveProgramminginKotlin2017Test/ReactiveProgramminginKotlin2017TestOrigin/Chapter02/src/main/kotlin/com/rivuchk/packtpub/reactivekotlin/chapter02/Monads.kt
@@ -3,10 +3,6 @@ package com.rivuchk.packtpub.reactivekotlin.chapter02
 import io.reactivex.Maybe
 import io.reactivex.rxkotlin.subscribeBy
 
-/**
- * Created by rivuc on 15-07-2017.
- */
-
 fun main(args: Array<String>) {
     val maybeValue: Maybe<Int> = Maybe.just(14)//1
     maybeValue.subscribeBy(//2
@@ -21,3 +17,4 @@ fun main(args: Array<String>) {
             onSuccess = { println("Completed with value $it")}//6
     )
 }
+
