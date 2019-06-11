a61f0116832c6e38a678be1860557703d501912a
Yun <yun.zhang5918@gmail.com>
2019-06-11 12:48:20 +0930

    change

--- oplTest/kotlinTest/rxKotlinTest/ReactiveProgramminginKotlin2017Test/ReactiveProgramminginKotlin2017TestOrigin/Chapter02/src/main/kotlin/com/rivuchk/packtpub/reactivekotlin/chapter02/PureFuncExmpl.kt
+++ oplTest/kotlinTest/rxKotlinTest/ReactiveProgramminginKotlin2017Test/ReactiveProgramminginKotlin2017TestOrigin/Chapter02/src/main/kotlin/com/rivuchk/packtpub/reactivekotlin/chapter02/PureFuncExmpl.kt
@@ -9,7 +9,7 @@ fun square(n:Int):Int {//(1)
 }
 
 fun main(args: Array<String>) {
-    println("named pure func square = ${com.rivuchk.packtpub.reactivekotlin.chapter02.square(3)}")
+    println("named pure func square = ${square(3)}")
     val qube = {n:Int -> n*n*n}//(2)
     println("lambda pure func qube = ${qube(3)}")
 }
