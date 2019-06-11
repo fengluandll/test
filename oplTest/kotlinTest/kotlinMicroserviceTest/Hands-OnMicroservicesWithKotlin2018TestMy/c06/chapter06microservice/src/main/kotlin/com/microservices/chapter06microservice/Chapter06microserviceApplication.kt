23d5b13ba716c1071227d756bbb54c2a87b802be
Yun <yun.zhang5918@gmail.com>
2019-06-11 16:50:41 +0930

    change

---
+++ oplTest/kotlinTest/kotlinMicroserviceTest/Hands-OnMicroservicesWithKotlin2018TestMy/c06/chapter06microservice/src/main/kotlin/com/microservices/chapter06microservice/Chapter06microserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.microservices.chapter06microservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Chapter06microserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<Chapter06microserviceApplication>(*args)
+}
