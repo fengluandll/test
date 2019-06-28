6c5ac85af47a6a5a0a43b567c97c9c2a1a3e4fd6
Yun <yun.zhang5918@gmail.com>
2019-06-28 17:18:07 +0930

    change

--- javaSETest/javaFrameworkTest/reactorTest/reactorBookTest/Hands-OnReactiveProgrammingwithReactor2018Test/Hands-OnReactiveProgrammingwithReactor2018TestM/c05/FibonacciWebFlux/src/main/java/com/example/fibwebflux/FibWebFluxApplication.java
+++ javaSETest/javaFrameworkTest/reactorTest/reactorBookTest/Hands-OnReactiveProgrammingwithReactor2018Test/Hands-OnReactiveProgrammingwithReactor2018TestM/c05/FibonacciWebFlux/src/main/java/com/example/fibwebflux/FibWebFluxApplication.java
@@ -2,8 +2,10 @@ package com.example.fibwebflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class FibWebFluxApplication {
 
     public static void main(String[] args) {
