d3090accf00e6c315eaf22052b660c72912d08d5
Yun <yun.zhang5918@gmail.com>
2019-06-28 16:44:49 +0930

    change

---
+++ javaSETest/javaFrameworkTest/reactorTest/reactorBookTest/Hands-OnReactiveProgrammingwithReactor2018Test/Hands-OnReactiveProgrammingwithReactor2018TestM/c05/FibonacciWebFlux/src/main/java/com/example/fibwebflux/FibWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.fibwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibWebFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FibWebFluxApplication.class, args);
+    }
+
+}
---
+++ javaSETest/javaFrameworkTest/reactorTest/reactorBookTest/Hands-OnReactiveProgrammingwithReactor2018Test/Hands-OnReactiveProgrammingwithReactor2018TestM/c05/FibonacciWebFlux/src/test/java/com/example/fibwebflux/FibWebFluxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.fibwebflux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FibWebFluxApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
