e3dd14a4b05d8b14c95d61fab39568b900c46d21
Yun <yun.zhang5918@gmail.com>
2019-06-28 16:52:41 +0930

    change

--- javaSETest/javaFrameworkTest/reactorTest/reactorBookTest/Hands-OnReactiveProgrammingwithReactor2018Test/Hands-OnReactiveProgrammingwithReactor2018TestO/Chapter04/reactorService/src/test/java/ReactorTest.java
+++ javaSETest/javaFrameworkTest/reactorTest/reactorBookTest/Hands-OnReactiveProgrammingwithReactor2018Test/Hands-OnReactiveProgrammingwithReactor2018TestO/Chapter04/reactorService/src/test/java/ReactorTest.java
@@ -41,7 +41,8 @@ public class ReactorTest {
     @Test
     public void testProcessor() {
         DirectProcessor<Long> data = DirectProcessor.create();
-        data.take(2).subscribe(t -> System.out.println(t));
+        data.take(2)
+                .subscribe(System.out::println);
         data.onNext(10L);
         data.onNext(11L);
         data.onNext(12L);
@@ -119,19 +120,20 @@ public class ReactorTest {
     @Test
     public void testReplayProcessor() {
         ReplayProcessor<Long> data = ReplayProcessor.create(3);
-        data.subscribe(t -> System.out.println(t));
+        data.subscribe(t -> System.out.println("s1: " + t));
         data.onNext(10L);
         data.onNext(11L);
         data.onNext(12L);
         data.onNext(13L);
         data.onNext(14L);
-        data.subscribe(t -> System.out.println(t));
+        data.subscribe(t -> System.out.println("s2: " + t));
     }
 
     @Test
     public void testTopicProcessor() {
         TopicProcessor<Long> data = TopicProcessor.<Long>builder()
-                .executor(Executors.newFixedThreadPool(2)).build();
+                .executor(Executors.newFixedThreadPool(2))
+                .build();
         data.subscribe(t -> System.out.println(t));
         data.subscribe(t -> System.out.println(t));
         FluxSink<Long> sink = data.sink();
@@ -142,7 +144,8 @@ public class ReactorTest {
 
     @Test
     public void testWorkQueueProcessor() {
-        WorkQueueProcessor<Long> data = WorkQueueProcessor.<Long>builder().build();
+        WorkQueueProcessor<Long> data = WorkQueueProcessor
+                .<Long>builder().build();
         data.subscribe(t -> System.out.println("1. " + t));
         data.subscribe(t -> System.out.println("2. " + t));
         FluxSink<Long> sink = data.sink();
@@ -180,10 +183,15 @@ public class ReactorTest {
                 () -> Tuples.<Long, Long>of(0L, 1L),
                 (state, sink) -> {
                     sink.next(state.getT1());
-                    return Tuples.of(state.getT2(), state.getT1() + state.getT2());
+                    return Tuples.of(state.getT2(),
+                            state.getT1() + state.getT2());
                 });
-        fibonacciGenerator.take(5).subscribe(t -> System.out.println("1. " + t));
-        fibonacciGenerator.take(5).subscribe(t -> System.out.println("2. " + t));
+        fibonacciGenerator
+                .take(5)
+                .subscribe(t -> System.out.println("1. " + t));
+        fibonacciGenerator
+                .take(5)
+                .subscribe(t -> System.out.println("2. " + t));
 
     }
 }
