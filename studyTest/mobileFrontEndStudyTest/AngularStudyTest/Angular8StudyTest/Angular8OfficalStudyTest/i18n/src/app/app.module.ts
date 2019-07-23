1449e685cea3019617da311b643296ad54cca95b
Yun <yun.zhang5918@gmail.com>
2019-07-24 07:29:33 +0930

    change

--- studyTest/mobileFrontEndStudyTest/AngularStudyTest/Angular8StudyTest/Angular8OfficalStudyTest/i18n/src/app/app.module.ts
+++ studyTest/mobileFrontEndStudyTest/AngularStudyTest/Angular8StudyTest/Angular8OfficalStudyTest/i18n/src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr'
+
 import { AppComponent } from './app.component';
+
+registerLocaleData(localeFr, "fr")
 
 @NgModule({
   imports: [ BrowserModule ],
