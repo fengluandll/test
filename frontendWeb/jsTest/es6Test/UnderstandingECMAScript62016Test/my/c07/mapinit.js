d3cebe59d8c436441e27552aff4c69a12825d66c
Yun <yun.zhang5918@gmail.com>
2019-08-05 17:31:32 +0930

    change

---
+++ frontendWeb/jsTest/es6Test/UnderstandingECMAScript62016Test/my/c07/mapinit.js
@@ -0,0 +1,8 @@
+let map = [["name", "Nicholas"], ["age", 25]];
+
+console.log(map.has('name'));
+console.log(map.get("name"));   // "Nicholas"
+console.log(map.has("age"));    // true
+console.log(map.get("age"));    // 25
+console.log(map.size);          // 2
+
