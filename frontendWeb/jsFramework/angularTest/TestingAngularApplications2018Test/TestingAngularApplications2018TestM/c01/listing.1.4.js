65bed1bc5874212ca80101f44040c794d3667dc5
yunzhang <yun.zhang5918@gmail.com>
2019-10-18 17:08:33 +1030

    change

---
+++ frontendWeb/jsFramework/angularTest/TestingAngularApplications2018Test/TestingAngularApplications2018TestM/c01/listing.1.4.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var protractor_1 = require("protractor");
+describe('Contacts App title test', function () {
+    it('Title should be correct', function () {
+        var appUrl = 'https://testing-angular-applications.github.io/';
+        var expectedTitle = 'Contacts App Starter';
+        protractor_1.browser.get(appUrl);
+        protractor_1.browser.getTitle().then(function (actualTitle) {
+            expect(actualTitle).toEqual(expectedTitle);
+        });
+    });
+});
