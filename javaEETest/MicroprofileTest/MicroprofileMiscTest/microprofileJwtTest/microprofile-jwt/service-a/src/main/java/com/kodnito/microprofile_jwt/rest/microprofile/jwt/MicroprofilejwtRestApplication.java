2cc18979bb0f77626aab82aaf51c2969eaad8149
yunzhang <yun.zhang5918@gmail.com>
2019-11-25 11:56:24 +1030

    add

---
+++ javaEETest/MicroprofileTest/MicroprofileMiscTest/microprofileJwtTest/microprofile-jwt/service-a/src/main/java/com/kodnito/microprofile_jwt/rest/microprofile/jwt/MicroprofilejwtRestApplication.java
@@ -0,0 +1,13 @@
+package com.kodnito.microprofile_jwt.rest.microprofile.jwt;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+@ApplicationScoped
+public class MicroprofilejwtRestApplication extends Application {
+}
